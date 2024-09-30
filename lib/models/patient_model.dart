9981541b3e38a039b87633e0fe8283393cd5afac
Aryan <aupathak1234@gmail.com>
2024-09-30 11:39:41 +0530

    updated patient model

---
+++ lib/models/patient_model.dart
@@ -0,0 +1,61 @@
+class Patient {
+  final String uid;
+  final String name;
+  final int age;
+  final String userType;
+  final String gender;
+  final String? profilePic;
+  final int phoneNumber;
+  final String email;
+  final String? bloodGroup;
+  final List<String?> allergies;
+  final List<String?> medications;
+  final int? emergencyContact;
+  final bool isSmoker;
+  final double height;
+  final double weight;
+
+
+  Patient({required this.uid, required this.name, required this.age, required this.userType, this.profilePic, required this.gender
+  , required this.phoneNumber, required this.email, this.bloodGroup, required this.allergies, required this.medications, this.emergencyContact, required this.isSmoker, required this.height, required this.weight});
+
+  factory Patient.fromJson(Map<String, dynamic> json) {
+    return Patient(
+      uid: json['uid'],
+      name: json['name'],
+      age: json['age'],
+      userType: json['userType'],
+      profilePic: json['profilePic'],
+      gender: json['gender'],
+      phoneNumber: json['phoneNumber'],
+      email: json['email'],
+      bloodGroup: json['bloodGroup'],
+      allergies: json['allergies'],
+      medications: json['medications'],
+      emergencyContact: json['emergencyContact'],
+      isSmoker: json['isSmoker'],
+      height: json['height'],
+      weight: json['weight'],
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'uid': uid,
+      'name': name,
+      'age': age,
+      'userType': userType,
+      'profilePic': profilePic,
+      'gender': gender,
+      'phoneNumber': phoneNumber,
+      'email': email,
+      'bloodGroup': bloodGroup,
+      'allergies': allergies,
+      'medications': medications,
+      'emergencyContact': emergencyContact,
+      'isSmoker': isSmoker,
+      'height': height,
+      'weight': weight,
+    };
+  }
+}
